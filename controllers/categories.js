a536896f437dedd921413f20906203976e55809b
abdullah <abdullamsuleman@gmail.com>
2026-08-10 11:32:57 +0300

    index controller

--- controllers/categories.js
+++ controllers/categories.js
@@ -9,7 +9,18 @@ res.status(201).json(category)
     }
 }
 
+const index = async (req, res) => {
+  try {
+    const categories = await Category.find({ user: req.user._id })
+    res.status(200).json(categories)
+  } catch (err) {
+    res.status(500).json({ err: err.message })
+  }
+}
+
 
 module.exports = {
     create,
+    index,
+
 }
